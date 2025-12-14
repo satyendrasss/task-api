403ef57adda1c362b2733814ebd4523d94430c87
Satyendra Singh <satyendrasinghbca777@gmail.com>
2025-12-14 18:21:41 +0530

    setup

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}`);
-// });
-
-module.exports = serverless(app);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
