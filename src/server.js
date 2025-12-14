50beab0004981321f3a5729f9cb9e58d9293e03c
Satyendra Singh <satyendrasinghbca777@gmail.com>
2025-12-14 17:32:18 +0530

    vercel setup

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+module.exports = serverless(app);
