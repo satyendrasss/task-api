e0a44ba92997f29d9971f9962bebf22ca8b2ad95
Satyendra Singh <satyendrasinghbca777@gmail.com>
2025-12-14 18:26:26 +0530

    working on deployment

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
+const serverless = require("serverless-http");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports = serverless(app);
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// });
